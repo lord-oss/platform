1dab2fc6c0e9a12ff49f6b59c3023f74cd9ef491
Amir Moldali <amir.s.moldali@mail.ru>
2025-06-22 23:00:04 +0500

    Fix duplicate sequelize declaration

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,26 @@
-// server/models/User.js
-
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database');
-const bcrypt = require('bcryptjs');
+const sequelize = require('./database'); // подключение из отдельного файла
 
 const User = sequelize.define('User', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true },
-  password: { type: DataTypes.STRING },
-  department: { type: DataTypes.STRING },
-  role: { type: DataTypes.STRING, defaultValue: 'employee' },
-});
-
-User.beforeCreate(async (user) => {
-  user.password = await bcrypt.hash(user.password, 10);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  department: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
 });
 
 module.exports = User;
-require('dotenv').config(); // подключаем .env
-const sequelize = require('./database');
 
-// после app.listen
-sequelize.authenticate()
-  .then(() => console.log('База данных подключена'))
-  .catch(err => console.error('Ошибка подключения к БД:', err));
 
